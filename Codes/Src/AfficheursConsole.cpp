92f403288df807647dbb2cd7747beb72bfbfe6f2
TarekCesi <boughzalatarek01.com>
2025-12-03 14:39:15 +0100

    Mise à jour de l'afficheur console à tester.

--- Codes/Src/AfficheursConsole.cpp
+++ Codes/Src/AfficheursConsole.cpp
@@ -1,25 +1,35 @@
 //
 // Created by silic on 01/12/2025.
 //
-
 #include "AfficheurConsole.hpp"
 #include <iostream>
+#include <vector>
 
-void AfficheurConsole::afficher(const Grille& g, int iterations) {
-    const int colonnes = g.getHauteur();
-    const int lignes = g.getLignes();
+void AfficheurConsole::afficher(const Grille& g, int iteration) {
 
-    std::cout << "Iteration" << std::endl;
-for (int y = 0; y < colonnes; ++y) {
-for (int x = 0; x < lignes; ++x) {
+    const int hauteur = g.getHauteur();  // nombre de lignes
+    const int largeur = g.getLargeur();  // nombre de colonnes
 
-    const Cellule& c = g.getCellule(x);
+    // Création de la matrice 0/1
+    std::vector<std::vector<int>> matrice(hauteur, std::vector<int>(largeur));
 
-    bool vivante = c.estVivante();
+    std::cout << "Iteration : " << iteration << "\n";
 
-    std::cout << (vivante ? 'X' : '.');
-}
-    std::cout << '\n';
-}
+    for (int y = 0; y < hauteur; ++y) {
+        for (int x = 0; x < largeur; ++x) {
+
+            const Cellule& c = g.getCellule(y, x);
+
+            bool vivante = c.estVivante();
+
+            // Affichage console (X = vivante, . = morte)
+            std::cout << (vivante ? 'X' : '.');
+
+            // Remplissage de la matrice 1/0
+            matrice[y][x] = vivante ? 1 : 0;
+        }
+        std::cout << '\n';
+    }
+
     std::cout << std::endl;
 }
