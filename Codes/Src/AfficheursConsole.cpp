bbda4078b4fa42a9faeea9a6c5e638ff986ddab7
TarekCesi <boughzalatarek01.com>
2025-12-03 12:02:33 +0100

    Mise à jour de l'afficheur console.

--- Codes/Src/AfficheursConsole.cpp
+++ Codes/Src/AfficheursConsole.cpp
@@ -6,5 +6,20 @@
 #include <iostream>
 
 void AfficheurConsole::afficher(const Grille& g, int iterations) {
-    // Affichage texte dans la console
+    const int colonnes = g.getHauteur();
+    const int lignes = g.getLignes();
+
+    std::cout << "Iteration" << std::endl;
+for (int y = 0; y < colonnes; ++y) {
+for (int x = 0; x < lignes; ++x) {
+
+    const Cellule& c = g.getCellule(x);
+
+    bool vivante = c.estVivante();
+
+    std::cout << (vivante ? 'X' : '.');
+}
+    std::cout << '\n';
+}
+    std::cout << std::endl;
 }
