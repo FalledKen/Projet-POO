81d1763ccd0f746d58d704348157771a66a9a817
Robin <porninrobin04@gmail.com>
2025-12-04 22:52:01 +0100

    Create TestsUnitaires.hpp

---
+++ Codes/Include/TestsUnitaires.hpp
@@ -0,0 +1,13 @@
+#ifndef TESTS_UNITAIRES_HPP
+#ifndef TESTS_UNITAIRES_HPP
+
+class TestsUnitaires{
+    public :
+        TestsUnitaires();
+        void testInitialisationGrille();
+        void testCompterVoisines();
+        void testEvolutionCellule();
+        void testLectureFichier();
+        void testEcritureFichier();
+    
+#endif TESTS_UNITAIRES_HPP
