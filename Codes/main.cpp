a6739ecd79e854e94788e659912620f5975529e9
Robin Pornin <porninrobin04@gmail.com>
2025-12-06 19:31:13 +0100

    correction du main.cpp

--- Codes/main.cpp
+++ Codes/main.cpp
@@ -10,23 +10,11 @@
 #include "TestsUnitaires.hpp"
 
 int main() {
-    //Jeu jeu;
-    //jeu.lancerSimulation();
+    Jeu jeu;
+    jeu.lancerSimulation();
     TestsUnitaires t;
-    //t.testLectureFichier();
+    t.testLectureFichier();
     t.testEcritureFichier();
-    // =====[ FICHIER ]=====
-    /*Fichier file;
-
-    const std::string nom_fichier = "grille";
-
-    const std::vector<std::vector<int>> matrice = file.lire_fichier(nom_fichier);
-
-    file.creer_dossier(nom_fichier);
-
-    for (int i = 0; i < 10; i++) {
-        file.ecrire_fichier(matrice, nom_fichier, i);
-    }*/
 
     return 0;
 }
